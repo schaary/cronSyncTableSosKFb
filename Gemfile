9d2c58a52ca3f3f1787bb7dd5cf7e0dda4950130
Michael Schaarschmidt <michael@schaaryworks.com>
2014-12-07 17:17:11 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+
+source 'https://rubygems.org'
+
+gem 'pg'
+gem 'ruby-plsql'
+gem 'sequel'
+gem 'thor'
---
+++ migrate/sos_k_fb_tbl.sql
@@ -0,0 +1,22 @@
+create table sos_k_fb_tbl (
+	fb varchar2(255),
+	aikz varchar2(255),
+	ktxt varchar2(255),
+	dtxt varchar2(255),
+	ltxt varchar2(255),
+	sbearb varchar2(255),
+	pbearb varchar2(255),
+	wahllokal varchar2(255),
+	wahltermin_von date,
+	wahltermin_bis date,
+	rueck_ort varchar2(255),
+	rueck_von date,
+	rueck_bis date,
+	sonst_termin1 date,
+	sonst_termin2 date,
+	sortkz1 varchar2(255),
+	sprache varchar2(255),
+	reffb varchar2(255)
+)
+/
+
